49d6c7064ba4c9034d2156f654bb1628068b8f42
gumacs92 <gguzmics01@gmail.com>
2015-07-17 19:41:31 +0200

    fade in reference

--- koll-art/javascript/main.js
+++ koll-art/javascript/main.js
@@ -3,10 +3,11 @@
  */
 var success = true;
 
+/*
 function validateEmail(email) {
-    var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+ var regex = "/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i";
     return re.test(email);
-}
+ }*/
 
 $(function(){
     //beincludeolas
@@ -26,44 +27,86 @@ $(function(){
 
 });
 
-
-$(document).ready(function(){
-    $("#send").click(function(){
-        //alert($("#message").val());
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var text = $("#message").val();
+function onbtnclick() {
+    //alert($("#message").val());
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var text = $("#message").val();
 
 
-        if (name == "") {
-            $("#namebox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-            $("#namediv").attr("class", "form-group has-error has-feedback");
-            success = false;
-        }else{
-            $("#namebox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-            $("#namediv").attr("class", "form-group has-success has-feedback");
-        }
-        if (email == ""){
-            $("#emailbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-            $("#emaildiv").attr("class", "form-group has-error has-feedback");
-            success = false
-        }else{
-            $("#emailbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-            $("#emaildiv").attr("class", "form-group has-success has-feedback");
-        }
-        if (text == "" || text == "Ide írd az üzeneted...") {
-            $("#textbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
-            $("#textdiv").attr("class", "form-group has-error has-feedback");
-            success = false;
-        }else{
-            $("#textbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
-            $("#textdiv").attr("class", "form-group has-success has-feedback");
-        }
+    if (name == "") {
+        $("#namebox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+        $("#namediv").attr("class", "form-group has-error has-feedback");
+        success = false;
+    } else {
+        $("#namebox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+        $("#namediv").attr("class", "form-group has-success has-feedback");
+    }
+    if (email == "") {
+        $("#emailbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+        $("#emaildiv").attr("class", "form-group has-error has-feedback");
+        success = false
+    } else {
+        $("#emailbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+        $("#emaildiv").attr("class", "form-group has-success has-feedback");
+    }
+    if (text == "" || text == "Ide írd az üzeneted...") {
+        $("#textbox").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+        $("#textdiv").attr("class", "form-group has-error has-feedback");
+        success = false;
+    } else {
+        $("#textbox").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+        $("#textdiv").attr("class", "form-group has-success has-feedback");
+    }
 
-        $('form').on('submit', function() {
-            return success;
-            alert(success);
-        });
+    $('form').on('submit', function () {
+        return success;
+        alert(success);
     });
+}
+
+function checkVisible(elm, eval) {
+    eval = eval || "visible";
+    var vpH = $(window).height(), // Viewport Height
+        st = $(window).scrollTop(), // Scroll Top
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height();
+
+    if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+    if (eval == "above") return ((y < (vpH + st)));
+}
+
+var reftomb;
+
+$(document).ready(function () {
+    reftomb = [$('#ref1'), $('#ref2'), $('#ref3'), $('#ref4'), $('#ref5'), $('#ref6')];
+    for (var i = 0; i < reftomb.length; i++) {
+        if (checkVisible(reftomb[i])) {
+            reftomb[i].css("webkitAnimationName", "megjelenes");
+            reftomb[i].css("webkitAnimationDuration", "4s");
+            reftomb[i].css("webkitanimationFillMode", "forwards");
+            //TODO többi böngészőre is....
+
+            reftomb[i].css("animationName", "megjelenes");
+            reftomb[i].css("animationDuration", "4s");
+            reftomb[i].css("animationFillMode", "forwards");
+        }
+    }
+});
+
+$(window).scroll(function () {
+    for (var i = 0; i < reftomb.length; i++) {
+        if (checkVisible(reftomb[i])) {
+            reftomb[i].show();
+            reftomb[i].css("webkitAnimationName", "megjelenes");
+            reftomb[i].css("webkitAnimationDuration", "4s");
+            reftomb[i].css("webkitanimationFillMode", "forwards");
+            //TODO itt is
+
+            reftomb[i].css("animationName", "megjelenes");
+            reftomb[i].css("animationDuration", "4s");
+            reftomb[i].css("animationFillMode", "forwards");
+        }
+    }
 });
 
