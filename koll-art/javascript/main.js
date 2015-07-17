5a4c77c31b1edf05b429a65b474219f31db2beb6
gumacs92 <gguzmics01@gmail.com>
2015-07-17 19:47:19 +0200

    Ez egy nagy pakk: Benne van a referencia előtünése A Web oldal  parallax hatása Az ezekhez tartozó módosítások a css és js fileokban A JS file átalakítása, hogy ne dobjon hibát, ha különböző oldalakon töltödik be az oldal például. Új képfilok hozzáadása tesztelési célókból (a netről szedett képeket majd törötlni kell) Új animate.css Néhány TODO megírása

--- koll-art/javascript/main.js
+++ koll-art/javascript/main.js
@@ -1,7 +1,8 @@
 /**
  * Created by gguzmics01 on 2015.07.07..
  */
-var success = true;
+
+//----------------------------------Segédfüggvények amiket az alsó függvények hívnak meg--------------------------------
 
 /*
 function validateEmail(email) {
@@ -9,23 +10,7 @@ function validateEmail(email) {
     return re.test(email);
  }*/
 
-$(function(){
-    //beincludeolas
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-
-    //carousel diavetiteshez
-    var slideqty = $('#featured .item').length;
-    for(var i = 0; i < slideqty; i++){
-        var insertText='<li data-target="#featured" data-slide-to="' + i+ '"></li>';
-        $('#featured ol').append(insertText);
-    }
-    $('.carousel').carousel({
-        interval:"7000",
-        pause:"false",
-    });
-
-});
+var success = true;
 
 function onbtnclick() {
     //alert($("#message").val());
@@ -71,42 +56,97 @@ function checkVisible(elm, eval) {
         st = $(window).scrollTop(), // Scroll Top
         y = $(elm).offset().top,
         elementHeight = $(elm).height();
-
+    //alert(y)
     if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
     if (eval == "above") return ((y < (vpH + st)));
 }
 
-var reftomb;
-
-$(document).ready(function () {
-    reftomb = [$('#ref1'), $('#ref2'), $('#ref3'), $('#ref4'), $('#ref5'), $('#ref6')];
+function addZoomIn(reftomb) {
     for (var i = 0; i < reftomb.length; i++) {
         if (checkVisible(reftomb[i])) {
-            reftomb[i].css("webkitAnimationName", "megjelenes");
-            reftomb[i].css("webkitAnimationDuration", "4s");
+            reftomb[i].css("webkitAnimationName", "zoomIn");
+            reftomb[i].css("webkitAnimationDuration", "1s");
             reftomb[i].css("webkitanimationFillMode", "forwards");
             //TODO többi böngészőre is....
 
-            reftomb[i].css("animationName", "megjelenes");
-            reftomb[i].css("animationDuration", "4s");
+            reftomb[i].css("animationName", "zoomIn");
+            reftomb[i].css("animationDuration", "1s");
             reftomb[i].css("animationFillMode", "forwards");
         }
     }
-});
+}
 
-$(window).scroll(function () {
-    for (var i = 0; i < reftomb.length; i++) {
-        if (checkVisible(reftomb[i])) {
-            reftomb[i].show();
-            reftomb[i].css("webkitAnimationName", "megjelenes");
-            reftomb[i].css("webkitAnimationDuration", "4s");
-            reftomb[i].css("webkitanimationFillMode", "forwards");
-            //TODO itt is
+function calcPos(winHeight, scroll, adjuster, inertia) {
+    return "50% " + (-((winHeight + scroll) - adjuster) * inertia) + "px";
+}
 
-            reftomb[i].css("animationName", "megjelenes");
-            reftomb[i].css("animationDuration", "4s");
-            reftomb[i].css("animationFillMode", "forwards");
-        }
+function moveBackground() {
+    var elem1 = $('.webimage1');
+    var elem2 = $('.webimage2');
+    var elem3 = $('.webimage3');
+    var scroll = $(window).scrollTop();
+    var winHeight = $(window).height();
+
+    if (checkVisible(elem1)) {
+        elem1.css({'backgroundPosition': calcPos(winHeight, scroll, winHeight, 0.05)});
     }
+    if (checkVisible(elem2)) {
+        //Tipp: itt azért kell osztani a 300-at 0.15-tel, hogy a calcPos függvényben történő szorzás ellenére is
+        //300px-vel tolja el a képet
+        elem2.css({'backgroundPosition': calcPos(winHeight, scroll, winHeight + 300 / 0.5, 0.5) + ", " + calcPos(winHeight, scroll, winHeight, 0.05)});
+    }
+    if (checkVisible(elem3)) {
+        elem3.css({'backgroundPosition': calcPos(winHeight, scroll, winHeight, 0.1)});
+    }
+}
+
+//-----------------------------------------HTML oldalakhoz tartozó függvények-------------------------------------------
+
+//esetleg külön js fileokba átrakni a külön oldalakhoz tartozó js kódokat... <<<< DONE
+//Tipp: összefogni pagenként egy függvénybe a pagehez tartozó függvényeket és meghívni az adott oldalon egy-egy
+//script tag között így: $(document).ready(function(){ aktualisHTMLfgv(); })
+
+//külön js filokba szednia különböző htmlekhez tartozó js kódokat, mert így hibát dob....
+//^^^^^^ megcsinálatam, de ehhez be kellett rakni konkrét script részeket a htmlbe^^^^^^
+//Megjegyzés: szerintem így viszont sokkal átláthatóbb lett az egész
+/*
+ $(document).ready(function () {});
+ $(window).scroll(function () {});
+ $(window).bind("onresize", function(){});
+ */
+
+//betöltő függvény minden page-re
+$(window).load(function () {
+    //beincludeolas
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
 });
+
+var loaded = false;
+var reftomb;
+
+function index() {
+    //carousel diavetiteshez
+    var slideqty = $('#featured .item').length;
+    for (var i = 0; i < slideqty; i++) {
+        var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+        $('#featured ol').append(insertText);
+    }
+    $('.carousel').carousel({
+        interval: "7000",
+        pause: "false",
+    });
+}
+
+function reference() {
+    if (!loaded) {
+        reftomb = [$('#ref1'), $('#ref2'), $('#ref3'), $('#ref4'), $('#ref5'), $('#ref6')];
+        loaded = true;
+    }
+    addZoomIn(reftomb);
+}
+
+function web() {
+    moveBackground();
+}
 
